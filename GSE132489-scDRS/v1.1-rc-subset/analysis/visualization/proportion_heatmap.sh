4515b849f824ef64a9aa4cebca99200582df3773
Xinzhe Li <lixinzhe@ucla.edu>
2025-06-18 11:41:13 -0700

    compiled, added plotting for gse132489 30K subset with different normalization

---
+++ GSE132489-scDRS/v1.1-rc-subset/analysis/visualization/proportion_heatmap.sh
@@ -0,0 +1,35 @@
+Rscript /u/home/l/lixinzhe/project-github/met-scDRS/GSE132489-scDRS/v1.1-rc-subset/analysis/visualization/proportion_heatmap_pipeline.R \
+    --dir "/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/v1.1/ges132489_30K_subset/mean_var_length/" \
+    --meta_data "/u/project/geschwind/lixinzhe/data/Liu_et_al_2021_methylation_gse132489/mch-30k-subset-meta.csv" \
+    --modality "mch-mean-var-length" \
+    --p_cutoff "0.1"
+
+Rscript /u/home/l/lixinzhe/project-github/met-scDRS/GSE132489-scDRS/v1.1-rc-subset/analysis/visualization/proportion_heatmap_pipeline.R \
+    --dir "/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/v1.1/ges132489_30K_subset/mean_var/" \
+    --meta_data "/u/project/geschwind/lixinzhe/data/Liu_et_al_2021_methylation_gse132489/mch-30k-subset-meta.csv" \
+    --modality "mch-mean-var" \
+    --p_cutoff "0.1"
+
+Rscript /u/home/l/lixinzhe/project-github/met-scDRS/GSE132489-scDRS/v1.1-rc-subset/analysis/visualization/proportion_heatmap_pipeline.R \
+    --dir "/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/v1.1/ges132489_30K_subset/mean_var_length_arcsine/" \
+    --meta_data "/u/project/geschwind/lixinzhe/data/Liu_et_al_2021_methylation_gse132489/mch-30k-subset-meta.csv" \
+    --modality "mch-mean-var-length-arcsine" \
+    --p_cutoff "0.1"
+
+Rscript /u/home/l/lixinzhe/project-github/met-scDRS/GSE132489-scDRS/v1.1-rc-subset/analysis/visualization/proportion_heatmap_pipeline.R \
+    --dir "/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/v1.1/ges132489_30K_subset/mean_var_length_arcsine/" \
+    --meta_data "/u/project/geschwind/lixinzhe/data/Liu_et_al_2021_methylation_gse132489/mch-30k-subset-meta.csv" \
+    --modality "mch-mean-var-length-arcsine" \
+    --p_cutoff "0.1"
+
+Rscript /u/home/l/lixinzhe/project-github/met-scDRS/GSE132489-scDRS/v1.1-rc-subset/analysis/visualization/proportion_heatmap_pipeline.R \
+    --dir "/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/v1.1/ges132489_30K_subset/mean_var_length_library/" \
+    --meta_data "/u/project/geschwind/lixinzhe/data/Liu_et_al_2021_methylation_gse132489/mch-30k-subset-meta.csv" \
+    --modality "mch-mean-var-length-library" \
+    --p_cutoff "0.1"
+
+Rscript /u/home/l/lixinzhe/project-github/met-scDRS/GSE132489-scDRS/v1.1-rc-subset/analysis/visualization/proportion_heatmap_pipeline.R \
+    --dir "/u/home/l/lixinzhe/project-geschwind/port/scratch/revision/v1.1/ges132489_30K_subset/mean_var_length_logit/" \
+    --meta_data "/u/project/geschwind/lixinzhe/data/Liu_et_al_2021_methylation_gse132489/mch-30k-subset-meta.csv" \
+    --modality "mch-mean-var-length-logit" \
+    --p_cutoff "0.1"
