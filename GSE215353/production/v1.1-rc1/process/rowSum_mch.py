c5087bf836ede65f8e16b7c5d2c98573701a9418
Xinzhe Li <lixinzhe@ucla.edu>
2025-07-01 01:07:40 -0700

    compiled

---
+++ GSE215353/production/v1.1-rc1/process/rowSum_mch.py
@@ -0,0 +1,45 @@
+### rowSum mcg ####################################################################################
+# load in libraries: 
+import pandas as pd
+import scanpy as sc
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime
+
+# load in the dataset:
+data_path = '/u/home/l/lixinzhe/project-geschwind/data/GSE215353/processed/production/processed-mch-v1_0_0_rc1.h5ad'
+adata = sc.read_h5ad(data_path)
+meta = pd.read_csv('/u/home/l/lixinzhe/project-geschwind/data/GSE215353/processed/production/meta_data.csv')
+system_date = datetime.date.today()
+subset_meta = pd.read_csv('/u/home/l/lixinzhe/project-geschwind/data/GSE215353/processed/meta/subset-mch-meta.csv')
+
+### PROCESS #######################################################################################
+# compute the row averge:
+row_sum = np.sum(adata.X, axis = 1)
+column_sum = np.sum(adata.X, axis = 0)
+
+# check if the cell names are correct:
+assert (adata.obs_names == meta.cell).all()
+
+# put the columns into meta:
+meta['rowSum'] = row_sum
+meta['log_scale_rowsum'] = np.log1p(row_sum) - np.mean(np.log1p(row_sum))
+meta['rowSum_centered'] = row_sum - np.mean(row_sum)
+
+# also outpuut cov:
+cov = pd.DataFrame({
+    'cell' : meta.cell,
+    'const': 1,
+    'log_scale_rowsum': meta.log_scale_rowsum
+})
+
+# also create one for 75k subset:
+subset_cov = cov.loc[cov.cell.isin(subset_meta.cell), :]
+
+# output the cov:
+cov.to_csv('/u/home/l/lixinzhe/project-geschwind/data/GSE215353/processed/production/full-mch-centered-log-library.cov', index = False)
+subset_cov.to_csv('/u/home/l/lixinzhe/project-geschwind/data/GSE215353/processed/production/subset75k-mch-centered-log-library.cov', index = False)
+
+# output the meta:
+meta.to_csv('/u/home/l/lixinzhe/project-geschwind/data/GSE215353/processed/production/meta_data_with_rowSum_mch.csv', index = False)
