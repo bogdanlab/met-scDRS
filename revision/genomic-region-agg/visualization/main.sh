77a52b1256ac4bcb32f61092bea681adf4009756
Xinzhe Li <lixinzhe@ucla.edu>
2026-07-14 11:29:57 -0700

    compiled, working script

--- revision/genomic-region-agg/visualization/main.sh
+++ revision/genomic-region-agg/visualization/main.sh
@@ -33,6 +33,15 @@ Rscript /u/home/l/lixinzhe/project-github/met-scDRS/revision/genomic-region-agg/
     --meta_data '/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/meta_data_50k.tsv' \
     --field "newL3" \
     --cutoff 0.1 \
+    --out "/u/home/l/lixinzhe/project-geschwind/plot/${current_date}-revision-GSE215353-50k-fraction-mch-cell-intron-type-significance-proportion_brain.png" \
+    --plot_type "proportion" \
+    --color_scale_max 0.5
+
+Rscript /u/home/l/lixinzhe/project-github/met-scDRS/revision/genomic-region-agg/visualization/proportion_auto.R \
+    --dir "/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/baseline_intron_CHN/" \
+    --meta_data '/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/meta_data_50k.tsv' \
+    --field "newL3" \
+    --cutoff 0.1 \
     --out "/u/home/l/lixinzhe/project-geschwind/plot/${current_date}-revision-GSE215353-50k-fraction-mch-cell-intron-type-significance-proportion.png" \
     --plot_type "proportion" \
     --color_scale_max 0.5
