24b1c410aef8e83d94ac3f39cf19287693de4b0e
Xinzhe Li <lixinzhe@ucla.edu>
2026-07-15 13:15:03 -0700

    compiled, working script for mch and mcg run single cell level for 50K subset

--- revision/genomic-region-agg/met-scDRS-run/non-CpG_run.sh
+++ revision/genomic-region-agg/met-scDRS-run/non-CpG_run.sh
@@ -47,3 +47,49 @@ for gs_file in ${input_gs_dir}KC_75_traits_split.gs*; do
     sleep 1
 
 done
+
+###########################################################################################
+######                                    QCed run                                   ######
+###########################################################################################
+submission_script="/u/home/l/lixinzhe/project-github/met-scDRS/revision/genomic-region-agg/met-scDRS-run/non-CpG_submission.sh"
+input_gs_dir="/u/home/l/lixinzhe/project-geschwind/port/scratch/parallel_gs/"
+
+for gs_file in ${input_gs_dir}KC_75_traits_split.gs*; do
+    # for each of the gs file submit a job:
+    echo "read gs file:"
+    echo "$gs_file"
+    
+    # compute scDRS:
+    qsub ${submission_script} \
+        '/u/project/geschwind/lixinzhe/data/GSE215353/extracted/merged_QCed/merged_071526_CHN_exon_QC.h5ad' \
+        "${gs_file}" \
+        "mean_var_length" \
+        "arcsine" \
+        "inv_std" \
+        '/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/qc_exon_CHN/sampling/' \
+        '/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/qc_exon_CHN/'
+    
+    # compute scDRS:
+    qsub ${submission_script} \
+        '/u/project/geschwind/lixinzhe/data/GSE215353/extracted/merged_QCed/merged_071526_CHN_promoter_QC.h5ad' \
+        "${gs_file}" \
+        "mean_var_length" \
+        "arcsine" \
+        "inv_std" \
+        '/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/qc_promoter_CHN/sampling/' \
+        '/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/qc_promoter_CHN/'
+    
+    # compute scDRS:
+    qsub ${submission_script} \
+        '/u/project/geschwind/lixinzhe/data/GSE215353/extracted/merged_QCed/merged_071526_CHN_intron_QC.h5ad' \
+        "${gs_file}" \
+        "mean_var_length" \
+        "arcsine" \
+        "inv_std" \
+        '/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/qc_intron_CHN/sampling/' \
+        '/u/home/l/lixinzhe/project-cluo/result/met-scDRS/revision/ges215353_50k/qc_intron_CHN/'
+    
+    # treat the cluster nicely:
+    sleep 1
+
+done
